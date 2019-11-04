37a81472bd34039e165ddc2916c515861f5c4457
Dustin Van Tate Testa <toast27@gmail.com>
2019-11-03 21:15:50 -0600

    comment, names

--- Sentiment/sentiment.py
+++ Sentiment/sentiment.py
@@ -1,19 +1,19 @@
 import json
 import textblob
 
-# returns average polarity of 
-def calculateSentiment(feed):	
+# returns average polarity (sentiment) and subjectivity (significance)
+def calculateSentiment(texts):	
 	out = [0.0, 0.0]
 	
-	for text in feed:
+	for text in texts:
 		blob = textblob.TextBlob(text['text'])
 		currentSentiment = blob.sentiment
 
 		out[0] += currentSentiment[0]
 		out[1] += currentSentiment[1]
 
-	out[0] /= len(feed)
-	out[1] /= len(feed)
+	out[0] /= len(texts)
+	out[1] /= len(texts)
 
 	return {
 		'sentiment' : out[0],
